dd70be90cc894be55c870800456dd8018c554335
ssz <331641539@qq.com>
2024-08-12 15:26:15 +0800

    追加excell

--- utils/lv_office/excel.go
+++ utils/lv_office/excel.go
@@ -159,11 +159,57 @@ func Write2Xls(filePath string, sheetName string, heads []string, listRows [][]s
 	}
 	// 设置单元格样式
 	//sheet.SetColWidth(5, 5, 60) // 设置单元格宽度 0-A 1-B 2-C
-	// 主体写入数据
-	for _, cells := range listRows {
+	if listRows != nil {
+		for _, cells := range listRows {
+			row := sheet.AddRow()
+			for _, cellData := range cells {
+				row.AddCell().Value = cellData
+			}
+		}
+	}
+	// 在提供的路径中将文件保存到xlsx文件
+	err = fileXls.Save(filePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// WriteMap2Xls 追加excell，由于map无序，必须通过数组传title以保证标题次序
+// header := []string{"title", "href", "content"}
+// title := map[string]string{"title": "标题", "href": "链接", "content": "内容"}
+func WriteMap2Xls(filePath string, sheetName string, heads []string, listRows []map[string]string, addHeader bool) error {
+	var fileXls *xlsx.File
+	if lv_file.IsFileExist(filePath) {
+		fileXls, err = xlsx.OpenFile(filePath)
+		sheet = fileXls.Sheet[sheetName]
+	} else {
+		err = CreateFilePath(filePath)
+		if err != nil {
+			logme.Error(err.Error())
+			return err
+		}
+		fileXls = xlsx.NewFile()
+		sheet, err = fileXls.AddSheet(sheetName)
+		if err != nil {
+			return err
+		}
+	}
+	// 头部写入
+	if heads != nil && addHeader {
 		row := sheet.AddRow()
-		for _, cellData := range cells {
-			row.AddCell().Value = cellData
+		for _, headV := range heads {
+			row.AddCell().Value = headV
+		}
+	}
+	// 设置单元格样式
+	//sheet.SetColWidth(5, 5, 60) // 设置单元格宽度 0-A 1-B 2-C
+	if listRows != nil {
+		for _, rowMap := range listRows {
+			row := sheet.AddRow()
+			for _, header := range heads { //按header的顺序添加
+				row.AddCell().Value = rowMap[header]
+			}
 		}
 	}
 	// 在提供的路径中将文件保存到xlsx文件
